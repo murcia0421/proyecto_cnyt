3c75a02ff8bd4371ac13436ae7969661754e14eb
murcia0421 <112002572+murcia0421@users.noreply.github.com>
2022-09-14 12:42:38 -0500

    Update lib 3.py

--- lib 3.py
+++ lib 3.py
@@ -1,1 +1,12 @@
-import lib2
+import numpy
+def canicas_booleanas():
+  m1 = np.array([[0,0,0,0,0,0],[0,0,0,0,0,0],[0,1,0,0,0.1],[0,0,0,1,0,0],{0,0,1,0,0,0],[1,0,0,0,1,0]])
+  n = int(input(" "))
+  final= np.dot(m1, m1)
+  for i in range(n-1):
+    matr = np.dot(final, final)                                                                                                                                           
+    final = matr                                                                      
+  return(final)
+                                                                          
+                                                                          
+                                                                          
